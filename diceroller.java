225349fd2ce7fbf46f661812e20f356da13338c4
Abigail Aleen Zachariah <abigail2005zachariah@gmail.com>
2025-11-05 11:57:23 +0530

    Dice roller
    This project accepts the number of dice to roll and uses Random class to generate values between 1 and 6. It then displays the result in an ASCII-style dice format.

---
+++ diceroller.java
@@ -0,0 +1,92 @@
+//This program rolls a given number of dice 
+import java.util.Random;
+import java.util.Scanner;
+
+public class diceroller {
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+        Random random=new Random();
+
+        int numOfDice;
+        int total=0;
+
+        System.out.print("Enter the number of dice:");
+        numOfDice=scanner.nextInt();
+
+        if(numOfDice>0){
+
+            for(int i=0;i<numOfDice;i++){
+                int roll=random.nextInt(1,7);
+                displaydie(roll);
+                System.out.println("Rolled value:"+roll);
+                total+=roll;
+            }
+            System.out.println("Total:"+total);
+        }
+        else{
+            System.out.println("Enter an input greater than zero");
+        }
+        scanner.close();
+    }
+    static void displaydie(int roll){
+
+        String dice1="""
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                """;
+
+        String dice2="""
+                 -------
+                | ●     |
+                |       |
+                |     ● |
+                 -------
+                """;
+        
+        String dice3="""
+                 -------
+                | ●     |
+                |   ●   |
+                |     ● |
+                 -------
+                """;
+        
+        String dice4="""
+                 -------
+                | ●   ● |
+                |       |
+                | ●   ● |
+                 -------
+                """;
+
+        String dice5="""
+                 -------
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                 -------
+                """;
+
+        String dice6="""
+                 -------
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                 -------
+                """;
+
+        switch(roll){
+            case 1-> System.out.print(dice1);
+            case 2-> System.out.print(dice2);
+            case 3-> System.out.print(dice3);
+            case 4-> System.out.print(dice4);
+            case 5-> System.out.print(dice5);
+            case 6-> System.out.print(dice6);
+            default -> System.out.println("INVALID ROLL");
+        }
+    }
+}
